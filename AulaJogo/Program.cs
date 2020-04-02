3fe92b48159ca0494d95b27a5bfae1364df61bc1
Junior Werner <juninhowerner31@gmail.com>
2020-04-02 14:09:33 -0300

    Projeto Criado

---
+++ AulaJogo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Tabuleiro;
+
+namespace AulaJogo
+{
+    //jogo de Xadrez
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao p = new Posicao(3,4);
+
+            Console.WriteLine(p);
+        }
+    }
+}
