32b8954a6aeb8f516b8d3dd3cf5bd0bca0371ff2
Junior Werner <juninhowerner31@gmail.com>
2020-04-02 14:39:04 -0300

    Projeto alterado (tabuleiro)

---
+++ AulaJogo/Tabuleiro/Cor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tabuleiro
+{
+    enum Cor
+    {
+        branca,
+        preta,
+        amarela,
+        vermelha,
+        verde,
+        Laranja
+    }
+}
