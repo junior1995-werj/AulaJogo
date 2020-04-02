8ca999993b06a8dfda402376c8dcf7127693fc81
Junior Werner <juninhowerner31@gmail.com>
2020-04-02 15:42:21 -0300

    Projeto alterado (colocando peças no tabuleiro2)

--- AulaJogo/Tabuleiro/Tabuleiro.cs
+++ AulaJogo/Tabuleiro/Tabuleiro.cs
@@ -16,14 +16,39 @@ namespace AulaJogo.tabuleiro
             Pecas = new Peca[linhas, colunas];
         }
 
+        public Peca peca(Posicao pos)
+        {
+            return Pecas[pos.Linha, pos.Coluna];
+        }
         public Peca peca(int linha, int coluna)
         {
             return Pecas[linha, coluna];
+        }
+        public bool ExistePeca(Posicao pos)
+        {
+            ValidarPosicao(pos);
+            return peca(pos) != null;
         }
         public void Colocarpeca(Peca p, Posicao pos)
         {
             Pecas[pos.Linha, pos.Coluna] = p;
             p.Posicao = pos;
+        }
+
+        public bool PosicaoValida(Posicao pos)
+        {
+            if (pos.Linha < 0 || pos.Linha > Linhas || pos.Coluna < 0 || pos.Coluna > Colunas)
+            {
+                return false;
+            }
+            return true;
+        }
+        public void ValidarPosicao(Posicao pos)
+        {
+            if (!PosicaoValida(pos))
+            {
+                throw new TabuleiroException("Posição invalida");
+            }
         }
     }
 }
---
+++ AulaJogo/Tabuleiro/TabuleiroException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AulaJogo.tabuleiro
+{
+    class TabuleiroException : Exception
+    {
+        public TabuleiroException (String mensagem) : base(mensagem)
+        {
+
+        }
+    }
+}
